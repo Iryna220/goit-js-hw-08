695474934334a73a36757adc7e80b68645660b6f
Iryna220 <ibiriszka951@gmail.com>
2023-06-13 22:50:28 +0200

    update home work 8
    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryContainer = document.querySelector(".gallery");
+const markup = galleryItems.map(({ preview, original, description }) => 
+`<li class="gallery__item">
+  <a class="gallery__link" href= ${original}>
+    <img
+      class="gallery__image"
+      src= ${preview}
+      data-source= ${original}
+      alt= ${description}
+    />
+  </a>
+</li>`
+)
+galleryContainer.insertAdjacentHTML('beforeend', markup.join(" "));
+galleryContainer.style.listStyle = "none";
+new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
+
+
