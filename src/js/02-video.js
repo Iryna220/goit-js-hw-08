5c4ad6a14a09c27fd4f04bf7be5f0806206c2ab4
Iryna220 <ibiriszka951@gmail.com>
2023-06-14 21:10:13 +0200

    update home work 8
    task 2, fixing

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+const onPlay = function(data) {
+    
+};
+
+player.on('play', onPlay);
+
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000)
+);
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time"))).then(function(seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
